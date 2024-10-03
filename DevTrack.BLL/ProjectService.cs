3b59d2957f569fa7817eaad382fe5611e168301a
Zajfan <114745912+Zajfan@users.noreply.github.com>
2024-10-03 05:02:59 +0200

    update

--- DevTrack.BLL/ProjectService.cs
+++ DevTrack.BLL/ProjectService.cs
@@ -1,33 +1,42 @@
-﻿public async Task<ProjectDashboardData> GetProjectDashboardDataAsync(int projectId)
-{
-    var project = await projectRepository.GetProjectByIdAsync(projectId);
-    var tasks = await taskRepository.GetTasksByProjectIdAsync(projectId);
-    // ... get other relevant data (e.g., milestones, documents, etc.)
+﻿using DevTrack.Models;
+using DevTrack.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-    return new ProjectDashboardData
+namespace DevTrack.BLL
+{
+    public class ProjectService
     {
-        Project = project,
-        Tasks = tasks,
-        // ... other data
-    };
-}
+        // Primary constructor for dependency injection
+        public ProjectService(ProjectRepository projectRepository, TaskRepository taskRepository /*, ... other repositories */)
+        {
+            this.projectRepository = projectRepository;
+            this.taskRepository = taskRepository;
+            // ... initialize other repositories
+        }
 
-// In ProjectRepository.cs (DAL)
-public async Task<Project> GetProjectByIdAsync(int projectId)
-{
-    // ... (Implementation to retrieve a single project by ID) ...
-}
+        public async Task<ProjectDashboardData> GetProjectDashboardDataAsync(int projectId)
+        {
+            var project = await projectRepository.GetProjectByIdAsync(projectId);
+            var tasks = await taskRepository.GetTasksByProjectIdAsync(projectId);
+            // ... get other relevant data (e.g., milestones, documents, etc.)
 
-// In TaskRepository.cs (DAL)
-public async Task<List<Task>> GetTasksByProjectIdAsync(int projectId)
-{
-    // ... (Implementation to retrieve tasks for a specific project) ...
-}
+            return new ProjectDashboardData
+            {
+                Project = project,
+                Tasks = tasks,
+                // ... other data
+            };
+        }
 
-// In ProjectDashboardData.cs (You'll need to create this class)
-public class ProjectDashboardData
-{
-    public Project Project { get; set; }
-    public List<Task> Tasks { get; set; }
-    // ... other properties to hold relevant data
+        // ... (Other methods in ProjectService) ...
+    }
+
+    // ProjectDashboardData.cs (This should be in the same file or a separate file in the BLL project)
+    public class ProjectDashboardData
+    {
+        public Project Project { get; set; }
+        public List<Task> Tasks { get; set; }
+        // ... other properties to hold relevant data
+    }
 }
